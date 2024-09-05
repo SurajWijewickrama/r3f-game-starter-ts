25b991bf14ecfa77e717c278a4a6679fca98aa07
SurajWijewickrama <wijewickramasuraj@gmail.com>
2024-09-05 15:02:09 +0530

    mobile joystick

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,69 @@
 import { StarterScene } from "./components/scenes/starter-scene";
 import UI from "./components/ui/ui";
-import { Suspense } from "react";
+import { Suspense, useRef, useState } from "react";
+import { useJoystickControls } from "ecctrl";
+import { Joystick } from "react-joystick-component";
+import { IJoystickUpdateEvent } from "react-joystick-component/build/lib/Joystick";
 
 export default function App() {
+  const pressButton1 = useJoystickControls((state) => state.pressButton1);
+  const setJoystick = useJoystickControls((state) => state.setJoystick);
+  const resetJoystick = useJoystickControls((state) => state.resetJoystick);
+  const releaseAllButtons = useJoystickControls(
+    (state) => state.releaseAllButtons
+  );
+  const joystick = useRef<Joystick>();
+
+  const handleMove = (e: IJoystickUpdateEvent) => {
+    console.log(e, Math.atan2(e.y, e.x));
+    setJoystick(e.distance, Math.atan2(e.y, e.x), true);
+  };
+  const handleStop = () => {
+    resetJoystick();
+    joystick.current.setState({
+      coordinates: {
+        relativeX: 0,
+        relativeY: 0,
+        distance: 0,
+        direction: null,
+        axisX: 0,
+        axisY: 0,
+      },
+      dragging: false,
+    });
+    console.log(joystick.current);
+  };
+
   return (
-    <>
+    <div className="h-full w-full overflow-clip">
       <UI>
         <Suspense fallback={null}>
+          <div className="lg:hidden flex absolute bottom-5 left-5 z-10">
+            <Joystick
+              size={100}
+              sticky={true}
+              baseColor="red"
+              stickColor="blue"
+              move={handleMove}
+              stop={handleStop}
+              ref={joystick}
+            ></Joystick>
+          </div>
+          <div className="lg:hidden flex absolute bottom-10 right-5 z-10">
+            <button
+              onPointerDown={() => {
+                pressButton1();
+              }}
+              onPointerUp={() => {
+                releaseAllButtons();
+              }}
+            >
+              Jump
+            </button>
+          </div>
           <StarterScene />
         </Suspense>
       </UI>
-    </>
+    </div>
   );
 }
--- src/components/scenes/starter-scene.tsx
+++ src/components/scenes/starter-scene.tsx
@@ -1,15 +1,38 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
 import { Canvas } from "@react-three/fiber";
-import { PointerEvent, Suspense } from "react";
+import { PointerEvent, Suspense, useEffect, useRef } from "react";
 import { StarterMap } from "../maps/starter-map";
 import { CharacterController } from "../character/character-controller";
 import { Perf } from "r3f-perf";
 import { Vector3 } from "three";
 
+// Helper function to check for WebGL 2.0 support
+const createWebGLContext = (
+  canvas: HTMLCanvasElement
+): WebGLRenderingContext | WebGL2RenderingContext | null => {
+  if (window.WebGL2RenderingContext) {
+    return canvas.getContext("webgl2"); // Try WebGL 2.0
+  } else {
+    return canvas.getContext("webgl"); // Fallback to WebGL 1.0
+  }
+};
+
 export const StarterScene = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const context = createWebGLContext(canvasRef.current);
+      if (!context) {
+        console.error("WebGL is not supported by this browser.");
+      }
+    }
+  }, []);
+
   return (
     <Canvas
+      ref={canvasRef}
       shadows
       camera={{ position: [3, 30, 3], fov: 30 }}
       onPointerDown={(e: PointerEvent<HTMLDivElement>) => {
@@ -17,6 +40,7 @@ export const StarterScene = () => {
           (e.target as HTMLDivElement).requestPointerLock();
         }
       }}
+      className="h-screen w-screen"
     >
       <Perf position="top-left" />
       <OrbitControls />
